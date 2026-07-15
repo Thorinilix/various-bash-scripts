463db68f35f6eda6bfbdd4a48095ad6d5570f761
Alex Brassell <thorinilix@gmail.com>
2026-07-14 21:23:39 -0400

    Create new-install-arch.sh
    One click new install script. Dumb.

---
+++ new-install-arch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is one of my "distrohopping" scripts, even though I have been hopping between Arch-based scripts.
+
+# This one truly is simple, and it's kind of dumb, but whatever.
+
+sudo pacman -Syu bitwarden localsend firefox discord steam github-desktop code
